c12d5a3dfa5b89cfe0949dcead1e6d2b13189085
Dafa Raisya <dafaraisya.dr@gmail.com>
2021-07-07 23:43:54 +0700

    add local storage when register

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,44 +3,45 @@ import axios from 'axios';
 const API_URL = 'https://mage-its.com/api/v1/auth/';
 
 class AuthService {
-  login(user) {
-    return axios
-      .post(`${API_URL}login`, {
-        email: user.email,
-        password: user.password,
-      })
-      .then((response) => {
-        if (response.data.tokens) {
-          localStorage.setItem('user', JSON.stringify(response.data));
-        }
+	login(user) {
+		return axios
+			.post(`${API_URL}login`, {
+				email: user.email,
+				password: user.password,
+			})
+			.then((response) => {
+				if (response.data.tokens) {
+					localStorage.setItem('user', JSON.stringify(response.data));
+				}
 
-        return response.data;
-      });
-  }
-
-  logout() {
-    localStorage.removeItem('user');
-  }
-
-  register(user) {
-    return axios.post(`${API_URL}register`, user);
-  }
-
-  confirmEmail(id) {
-    return axios.put(`${API_URL}confirm-email/${id}`);
-  }
-
-  findByEmail(email) {
-    return axios.get(`${API_URL}find-by-email/${email}`);
-  }
-
-  requestChangePassword(email) {
-    return axios.post(`${API_URL}request-change-password/${email}`);
-  }
-
-  changePassword(user) {
-    return axios.put(`${API_URL}change-password/${user.id}`, user);
-  }
+				return response.data;
+			});
+	}
+	logout() {
+		localStorage.removeItem('user');
+	}
+	register(user) {
+		return axios
+			.post(`${API_URL}register`, user)
+			.then((response) => {
+				if (response.data.tokens) {
+					localStorage.setItem('user', JSON.stringify(response.data));
+				}
+				return response.data;
+			});
+	}
+	confirmEmail(id) {
+		return axios.put(`${API_URL}confirm-email/${id}`);
+	}
+	findByEmail(email) {
+		return axios.get(`${API_URL}find-by-email/${email}`);
+	}
+	requestChangePassword(email) {
+		return axios.post(`${API_URL}request-change-password/${email}`);
+	}
+	changePassword(user) {
+		return axios.put(`${API_URL}change-password/${user.id}`, user);
+	}
 }
 
 export default new AuthService();
